505ecba4fc4c256ae5d8df93fbdc8a17f0d9d817
luis lopez <llopez@elcatador.com>
2024-01-31 18:44:18 -0400

    Styling the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
-import './App.css';
+import { TodoList } from './Components/TodoList';
+import { TodoSearch } from './Components/TodoSearch';
+import { TodoItem } from './Components/TodoItem';
+import { CreateTodoButton } from './Components/CreateTodoButton';
+import React from 'react';
+import { TodoCounter } from './Components/TodoCounter';
+
+const defaultTodos = [
+  {text: "Cortar cebolla", completed: true,},
+  {text: "Completar platzi", completed: false,},
+  {text: "Llorar con la llorona", completed: false,},
+  {text: "Klk manito", completed: true,},
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <TodoCounter completed={4} total={15} ></TodoCounter>
+
+      <TodoSearch></TodoSearch>
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+        <TodoItem key={todo.text} text={todo.text} completed={todo.completed}></TodoItem>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton></CreateTodoButton>
+
+
+    </>
   );
 }
+
+
 
 export default App;
